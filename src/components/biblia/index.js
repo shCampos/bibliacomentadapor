7fec14b924dc46727cbf1968ceb5ae713d725be1
shcampos <samuel-hc@hotmail.com>
2021-02-12 21:39:37 -0400

    features: sckeleton of app created

--- src/components/biblia/index.js
+++ src/components/biblia/index.js
@@ -8,10 +8,21 @@ import { styleObject } from '../../assets/styleObject.js'
 
 export default function Biblia() {
   const classes = styleObject()
-
+  
   return (
     <Paper className={classes.middleContainers}>
       <Typography variant="h6">livro</Typography>
+      {
+        ['abc', 'dbsda', 'oihsdisd'].forEach((text, index) => {
+          console.log(index, text)
+          return (
+           <Typography variant="body2">
+             <sup>{index} </sup>
+             {text}
+           </Typography>
+          )
+        })
+      }
     </Paper>
   )
 }
